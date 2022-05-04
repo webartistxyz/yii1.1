dbd2352a6eb63488dee9bc74f7ff88de273d0b88
shaheen2013 <tasnif06@gmail.com>
2022-05-04 21:33:05 +0600

    practice yii1i1

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../Yii1/yii-1.1.25.43e386/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
